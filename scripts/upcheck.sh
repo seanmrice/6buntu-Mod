66a70c387b295ab07f5ffc84ea77aec186119c80
Sean Rice <sean.m.rice@gmail.com>
2011-03-29 18:53:10 -0700

    Adde some upcheck elements

--- scripts/upcheck.sh
+++ scripts/upcheck.sh
@@ -14,15 +14,21 @@ if [ -e /etc/6buntu ]
                 if grep $upcheck -ne "$version" >> /dev/null
                     then
                         sudo echo "$version" > $upcheck && echo "$time Version update file written successfully" >> $LOG
+                        echo "$version \n \l" > ./config/issue && sudo cp ./config/issue /etc/issue && echo "issue successfully updated" && echo "$time issue successfully updated" >> $LOG
+                        echo "$version" > ./config/issue.net && sudo cp ./config/issue.net /etc/issue.net && echo "issue.net successfully updated" && echo "$time issue.net successfully updated" >>$LOG
                     else
                         echo "$time 6buntu is up-to-date!" >> $LOG
                 fi
             else
                 sudo chmod 1775 /etc/6buntu
                 sudo echo "$version" > $upcheck && echo "$time upcheck successfully completed" >> $LOG
+                echo "$version \n \l" > ./config/issue && sudo cp ./config/issue /etc/issue && echo "issue successfully updated" && echo "$time issue successfully updated" >> $LOG
+                echo "$version" > ./config/issue.net && sudo cp ./config/issue.net /etc/issue.net && echo "issue.net successfully updated" && echo "$time issue.net successfully updated" >>$LOG
                 exit 300
         fi
     else
         sudo mkdir /etc/6buntu && sudo chmod 1777 /etc/6buntu && sudo echo "$version" > $upcheck && echo "$time Successfully created upcheck file" >> $LOG
+        echo "$version \n \l" > ./config/issue && sudo cp ./config/issue /etc/issue && echo "issue successfully updated" && echo "$time issue successfully updated" >> $LOG
+        echo "$version" > ./config/issue.net && sudo cp ./config/issue.net /etc/issue.net && echo "issue.net successfully updated" && echo "$time issue.net successfully updated" >>$LOG
 fi
 exit 0
